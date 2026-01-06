87e1e5d5373e21419119eb9500d76f9add7be574
Francois-Xavier Seingier <francoisxavier.seingier@broadpeak.tv>
2026-01-06 17:49:14 +0000

    Git blame popup

--- init.lua
+++ init.lua
@@ -329,6 +329,17 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gs = require 'gitsigns'
+
+        -- VS Code–style blame popup
+        vim.keymap.set('n', '<leader>gb', function()
+          gs.blame_line { full = true }
+        end, { buffer = bufnr, desc = 'Git blame line' })
+
+        -- Optional: toggle inline blame
+        vim.keymap.set('n', '<leader>gB', gs.toggle_current_line_blame, { buffer = bufnr, desc = 'Toggle inline blame' })
+      end,
     },
   },
 
